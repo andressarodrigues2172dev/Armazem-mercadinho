17d9cd0237b1878415265bda6f4672c0d5514ce6
Andressa Rodrigues <andressa.rodrigues2172@gmail.com>
2024-07-16 21:32:17 -0300

    Criação da arquitetura básica do projeto

---
+++ src/test/java/com/mercadinho/armazem/ArmazemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadinho.armazem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmazemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
